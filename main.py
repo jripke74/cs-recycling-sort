46c07a7cfbed82e62c6c77b58afaea198992325e
Jeff Ripke <jripke74@gmail.com>
2025-04-15 14:10:40 -0700

    add step 2 code

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ waste_type = "trash"
 
 if material == "plastic":
     waste_type = "recycling"
+if length < 7.5:
+    waste_type = "trash"
 
 print("Please deposit your item in the " + waste_type + " bin.")
